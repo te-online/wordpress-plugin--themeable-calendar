b43d10aeb9f6a8621e7dcc1db39d55c1fcb4504d
Thomas Ebert <thomas.ebert@te-online.net>
2016-11-24 22:52:10 +0100

    Make widget more dynamic with backend settings.

--- includes/class-te-calendar-widget.php
+++ includes/class-te-calendar-widget.php
@@ -26,17 +26,16 @@ class Te_Calendar_Widget extends WP_Widget {
 	 * @param array $args
 	 * @param array $instance
 	 */
-	public function widget( $args, $instance ) {
+	public function widget( $args, $instance = array( 'title' => '', 'num_events' => 5 ) ) {
 		// outputs the content of the widget
-		// $title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
-		// $num_posts = ( empty( $instance['num_posts'] ) || intval( $instance['num_posts']) < 1 ) ? 3 : intval( $instance['num_posts'] );
+		$num_events = intval( $instance['num_events'] );
 
 		$today = strtotime( 'today midnight' );
-		// print_r(maybe_serialize($today));
 
 		query_posts( array(
-				'posts_per_page' => 5,
+				'posts_per_page' => $num_events,
 				'post_type' => 'tecal_events',
 				'orderby' => 'meta_value',
 				'meta_key' => 'tecal_events_begin',
@@ -52,7 +51,9 @@ class Te_Calendar_Widget extends WP_Widget {
 			)
 		);
 
-		 // echo $GLOBALS['wp_query']->request;
+		if( !empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
 
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'templates/te-calendar-default-template-sidebar.php';
 
@@ -72,14 +73,14 @@ class Te_Calendar_Widget extends WP_Widget {
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
 
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Number of events to display:'); ?></label>
+		<p><label for="<?php echo $this->get_field_id('num_events'); ?>"><?php _e('Number of events to display:'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('num_events'); ?>" name="<?php echo $this->get_field_name('num_events'); ?>" type="text" value="<?php echo esc_attr($num_events); ?>" /></p>
 
-		<p>Here goes a setting for future/past events</p>
+		<!-- <p>Here goes a setting for future/past events</p>
 
 		<p>Here goes a setting for the direction to go in (order)</p>
 
-		<p>Here goes a select field to select a template.</p>
+		<p>Here goes a select field to select a template.</p> -->
 		<?php
 	}
 
