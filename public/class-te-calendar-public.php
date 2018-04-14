502322a669bad3dff209a7473b22052b0f8d58d2
Thomas Ebert <thomas.ebert@te-online.net>
2018-04-14 21:35:50 +0200

    Don't enqueue script or style in public for the plugin, since those files are empty.

--- public/class-te-calendar-public.php
+++ public/class-te-calendar-public.php
@@ -73,7 +73,7 @@ class Te_Calendar_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/te-calendar-public.css', array(), $this->version, 'all' );
+		// wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/te-calendar-public.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Te_Calendar_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/te-calendar-public.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/te-calendar-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
