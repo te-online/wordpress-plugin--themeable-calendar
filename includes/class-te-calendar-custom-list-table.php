395877242783f9d2b810ad9d3429755591c8c624
Thomas Ebert <thomas.ebert@te-online.net>
2018-04-15 00:30:23 +0200

    Add tab bar to switch between list view and calendar view. Fixes #15.

--- includes/class-te-calendar-custom-list-table.php
+++ includes/class-te-calendar-custom-list-table.php
@@ -15,73 +15,87 @@ class Te_Calendar_Custom_List_Table extends WP_Posts_List_Table {
 
     // Your custom list table is here
     public function display() {
+    	// Load calendars.
     	$calendars = get_terms( 'tecal_calendars' );
+    	// See if we are in list view or calendar view.
+    	$listView = ('list' === get_query_var('view')) ? true : false;
 
     	?>
-    	<div class="tecal__edit-modal__container">
-	    	<div class="tecal__edit-modal__inner">
-					<h3 class="tecal__edit-modal__new-title"><?php _e( 'New event', 'te-calendar' ); ?></h3>
-					<h3 class="tecal__edit-modal__edit-title"><?php _e( 'Edit event', 'te-calendar' ); ?></h3>
-					<p class="tecal__edit-modal__error"></p>
-					<form action="" method="post" id="edit-event-form">
+    	<h2 class="nav-tab-wrapper">
+		    <a href="?post_type=tecal_events" class="nav-tab<?php echo (!$listView) ? ' nav-tab-active' : ''; ?>">
+		    	<?php _e('Calendar view', 'te-calendar'); ?>
+		    </a>
+		    <a href="?post_type=tecal_events&view=list" class="nav-tab<?php echo ($listView) ? ' nav-tab-active' : ''; ?>">
+		    	<?php _e('List view', 'te-calendar'); ?>
+		    </a>
+			</h2>
 
-						<p><label for="tecal_events_title"><?php _e('Title:', 'te-calendar'); ?></label>
-						<input class="widefat" id="tecal_events_title" name="tecal_events_title" type="text" value="" /></p>
+			<?php if(!$listView): ?>
 
-						<p><label for="tecal_events_allday"><?php _e('All day:', 'te-calendar'); ?></label>
-						<input class="widefat" id="tecal_events_allday" name="tecal_events_allday" type="checkbox" /></p>
+	    	<div class="tecal__edit-modal__container">
+		    	<div class="tecal__edit-modal__inner">
+						<h3 class="tecal__edit-modal__new-title"><?php _e( 'New event', 'te-calendar' ); ?></h3>
+						<h3 class="tecal__edit-modal__edit-title"><?php _e( 'Edit event', 'te-calendar' ); ?></h3>
+						<p class="tecal__edit-modal__error"></p>
+						<form action="" method="post" id="edit-event-form">
 
-				    <p class="tecal_multi-input"><label for="tecal_events_begin"><?php _e('Begin:', 'te-calendar'); ?>&nbsp;</label>
-						<input class="threethirdfat" id="tecal_events_begin" name="tecal_events_begin" type="text" value="" />
-						<input class="thirdfat" id="tecal_events_begin_time" name="tecal_events_begin_time" type="text" value="" /></p>
+							<p><label for="tecal_events_title"><?php _e('Title:', 'te-calendar'); ?></label>
+							<input class="widefat" id="tecal_events_title" name="tecal_events_title" type="text" value="" /></p>
 
-						<p><label for="tecal_events_has_end"><?php _e('Specify an end:', 'te-calendar'); ?></label>
-						<input class="widefat" id="tecal_events_has_end" name="tecal_events_has_end" type="checkbox" /></p>
+							<p><label for="tecal_events_allday"><?php _e('All day:', 'te-calendar'); ?></label>
+							<input class="widefat" id="tecal_events_allday" name="tecal_events_allday" type="checkbox" /></p>
 
-						<p class="tecal_multi-input"><label for="tecal_events_end"><?php _e('End:', 'te-calendar'); ?>&nbsp;</label>
-						<input class="threethirdfat" id="tecal_events_end" name="tecal_events_end" type="text" value="" />
-						<input class="thirdfat" id="tecal_events_end_time" name="tecal_events_end_time" type="text" value="" /></p>
+					    <p class="tecal_multi-input"><label for="tecal_events_begin"><?php _e('Begin:', 'te-calendar'); ?>&nbsp;</label>
+							<input class="threethirdfat" id="tecal_events_begin" name="tecal_events_begin" type="text" value="" />
+							<input class="thirdfat" id="tecal_events_begin_time" name="tecal_events_begin_time" type="text" value="" /></p>
 
-				    <p><label for="tecal_events_location"><?php _e('Location:', 'te-calendar'); ?></label>
-						<input class="widefat" id="tecal_events_location" name="tecal_events_location" type="text" value="" /></p>
+							<p><label for="tecal_events_has_end"><?php _e('Specify an end:', 'te-calendar'); ?></label>
+							<input class="widefat" id="tecal_events_has_end" name="tecal_events_has_end" type="checkbox" /></p>
 
-						<p><label for="tecal_events_description"><?php _e('Description:', 'te-calendar'); ?></label>
-						<textarea class="widefat tecal_events_description" id="tecal_events_description" name="tecal_events_description"></textarea></p>
+							<p class="tecal_multi-input"><label for="tecal_events_end"><?php _e('End:', 'te-calendar'); ?>&nbsp;</label>
+							<input class="threethirdfat" id="tecal_events_end" name="tecal_events_end" type="text" value="" />
+							<input class="thirdfat" id="tecal_events_end_time" name="tecal_events_end_time" type="text" value="" /></p>
 
-						<p><label for="tecal_events_calendar"><?php _e('Calendar:', 'te-calendar'); ?></label>
-						<select class="widefat" id="tecal_events_calendar" name="tecal_events_calendar">
-							<?php foreach( $calendars as $calendar ) { ?>
-								<option value="<?php echo $calendar->slug; ?>"><?php echo $calendar->name; ?></option>
-							<?php } ?>
-						</select>
-						</p>
+					    <p><label for="tecal_events_location"><?php _e('Location:', 'te-calendar'); ?></label>
+							<input class="widefat" id="tecal_events_location" name="tecal_events_location" type="text" value="" /></p>
 
-						<div class="tecal_edit-modal__submit">
-							<input type="hidden" name="tecal_events_edit_id" value="" />
+							<p><label for="tecal_events_description"><?php _e('Description:', 'te-calendar'); ?></label>
+							<textarea class="widefat tecal_events_description" id="tecal_events_description" name="tecal_events_description"></textarea></p>
 
-							<input type="button" name="tecal_edit-modal_cancel" value="<?php _e( 'Cancel', 'te-calendar' ); ?>" class="button-secondary" />
+							<p><label for="tecal_events_calendar"><?php _e('Calendar:', 'te-calendar'); ?></label>
+							<select class="widefat" id="tecal_events_calendar" name="tecal_events_calendar">
+								<?php foreach( $calendars as $calendar ) { ?>
+									<option value="<?php echo $calendar->slug; ?>"><?php echo $calendar->name; ?></option>
+								<?php } ?>
+							</select>
+							</p>
 
-							<input type="button" name="tecal_edit-modal_delete" value="<?php _e( 'Delete', 'te-calendar' ); ?>" data-defaultcaption="<?php _e( 'Delete', 'te-calendar' ); ?>" data-busycaption="<?php _e( 'Deleting...', 'te-calendar' ); ?>" class="button-secondary" />
+							<div class="tecal_edit-modal__submit">
+								<input type="hidden" name="tecal_events_edit_id" value="" />
 
-							<input type="submit" name="tecal_edit-modal_save" value="<?php _e( 'Save', 'te-calendar' ); ?>" data-defaultcaption="<?php _e( 'Save', 'te-calendar' ); ?>" data-busycaption="<?php _e( 'Saving...', 'te-calendar' ); ?>" class="button-primary" />
-						</div>
+								<input type="button" name="tecal_edit-modal_cancel" value="<?php _e( 'Cancel', 'te-calendar' ); ?>" class="button-secondary" />
 
-					</form>
+								<input type="button" name="tecal_edit-modal_delete" value="<?php _e( 'Delete', 'te-calendar' ); ?>" data-defaultcaption="<?php _e( 'Delete', 'te-calendar' ); ?>" data-busycaption="<?php _e( 'Deleting...', 'te-calendar' ); ?>" class="button-secondary" />
+
+								<input type="submit" name="tecal_edit-modal_save" value="<?php _e( 'Save', 'te-calendar' ); ?>" data-defaultcaption="<?php _e( 'Save', 'te-calendar' ); ?>" data-busycaption="<?php _e( 'Saving...', 'te-calendar' ); ?>" class="button-primary" />
+							</div>
+
+						</form>
+					</div>
 				</div>
-			</div>
 
-			<?php if('list' != get_query_var('view')): ?>
 				<div id="calendar"></div>
+
+			<?php else: ?>
+
+				<div class="tecal_list_table">
+					<?php
+						// parent::search_box();
+						parent::display();
+					?>
+				</div>
+
 			<?php endif; ?>
-
-
-			<div class="tecal_list_table">
-		<?php
-
-			// parent::search_box();
-      parent::display();
-     ?>
-     	</div>
      	<?php
     }
 }
